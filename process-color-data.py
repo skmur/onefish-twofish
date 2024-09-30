89f3f7d600d7afb1cf40e91218759dd7dc9a3647
smurthy <smurthy@holy7c24401.rc.fas.harvard.edu>
2024-09-29 23:01:53 -0400

    number of files for each model configuration

--- process-color-data.py
+++ process-color-data.py
@@ -1,6 +1,7 @@
 import pickle
 import numpy as np
 import pandas as pd
+import os
 
 # SCRATCHPAD: PROCESS AND COMPILE RAW DATA AS NEEDED AND SAVE TO FINAL FORMAT
 # output-data/50words-100subjs = initial subset of data for baselines containing "response" and "completion" task versions
@@ -20,44 +21,99 @@ def sanityCheck(df, num_subjects, num_words):
 #----------------------------------------------------------------------
 # TRASNFER FILES FROM STORAGE TO LOCAL AS NEEDED
 
-# loop through files in directory and unpickle and print out the df
-import os
-dir = "/n/holylabs/LABS/ullman_lab/Users/smurthy/onefish-twofish/output-data/concept-task/"
-for filename in os.listdir(dir):
-    if filename.endswith(".pickle"):
-        with open(dir + filename, 'rb') as f:
-            data = pickle.load(f)
-            print(filename)
-            if data is not None:
-                if ('generation1' in data.columns) and len(data) > 1000:
-                    print(filename)
-                    print(len(data))
-                    # copy file to new directory
-                    os.system("cp " + dir + filename + " ./output-data/concept-task/")
-                elif not filename.startswith("[test]"):
-                    # rename file by adding [test] to the beginning
-                    os.rename(dir + filename, dir + "[test]" + filename)
-                    print("renamed file: ", filename)
-                else: 
-                    continue
-                print("---------------------------------------------------")
+# # loop through files in directory and unpickle and print out the df
 
-
-# #----------------------------------------------------------------------
-# dir = "./output-data/concept-task/"
+# dir = "/n/holylabs/LABS/ullman_lab/Users/smurthy/onefish-twofish/output-data/concept-task/"
 # for filename in os.listdir(dir):
 #     if filename.endswith(".pickle"):
 #         with open(dir + filename, 'rb') as f:
 #             data = pickle.load(f)
+#             print(filename)
+#             if data is not None:
+#                 if ('generation1' in data.columns) and len(data) > 1000:
+#                     print(filename)
+#                     print(len(data))
+#                     print(data.columns)
+#                     # copy file to new directory
+#                     # os.system("cp " + dir + filename + " ./output-data/concept-task/")
+#                 elif not filename.startswith("[test]"):
+#                     # rename file by adding [test] to the beginning
+#                     # os.rename(dir + filename, dir + "[test]" + filename)
+#                     print("renamed file: ", filename)
+#                 else: 
+#                     continue
+#                 print("---------------------------------------------------")
 
-#             # group by and print number of responses per target word
-#             print(data.groupby(['target', 'choice1', 'choice2']).size())
 
+#----------------------------------------------------------------------
+models = ["starling", "openchat", "gemma-instruct", "zephyr-gemma", "mistral-instruct", "zephyr-mistral", "llama2", "llama2-chat"]
+lab_storage_dir = "/n/holylabs/LABS/ullman_lab/Users/smurthy/onefish-twofish/output-data/concept-task/"
+output_dir = "./output-data/concept-task/"
+
+# concatenate all dataframes by model
+for model in models:
+    concat = pd.DataFrame()
+    print("Processing model: ", model)
+    count = 0
+    for filename in os.listdir(lab_storage_dir):
+        if ("[test]" not in filename) and (model in filename) and (filename.endswith(".pickle")):
+            if model == "llama2":
+                #  check that "chat" is not in the filename
+                if "chat" in filename:
+                    continue
+                    
+            if filename == "concepts-zephyr-gemma-category=politicians-prompt=nonsense-temp=default.pickle":
+                continue
+
+            with open(lab_storage_dir + filename, 'rb') as f:
+                data = pickle.load(f)
+
+                if data is None:
+                    print("Alert: ", filename)
+                    continue
+
+                # extract the part of the filename that contains the prompt type
+                # strip .piclke from the filename
+                filename_tmp = filename.split(".pickle")[0]
+                params = filename_tmp.split("-")
+                for param in params:
+                    if "prompt" in param:
+                        prompt = param.split("=")[1]
+                    if "category" in param:
+                        category = param.split("=")[1]
+                    if "temp" in param:
+                        temperature = param.split("=")[1]
+                        
+                count+=1
+                print("-->", category, prompt, temperature, len(data))
+
+                # add the prompt type to the dataframe
+                data['prompt'] = prompt
+
+                # save the updated dataframe to lab_storage_dir
+                with open(lab_storage_dir + filename, 'wb') as f:
+                    pickle.dump(data, f)
+            
+                # concatenate all the dataframes for this model
+                if 'concat' in locals():
+                    concat = pd.concat([concat, data])
+            
+
+    print("total number of rows: ", len(concat))
+    print("total number of files: ", count)
+
+    if len(concat) == 0:
+        print("No data for model: ", model)
+    else: 
+        # save the concatenated dataframe
+        with open(output_dir + "concepts-" + model + ".pickle", 'wb') as f:
+            pickle.dump(concat, f)
+
+    print("---------------------------------------------------")
 
             
 # #----------------------------------------------------------------------
 # # CHECK ALL MODELS
-# models = ["starling", "openchat", "gemma-instruct", "zephyr-gemma", "mistral-instruct", "zephyr-mistral", "llama2", "llama2-chat"]
 
 # num_subjects = 150
 # num_words = 199
