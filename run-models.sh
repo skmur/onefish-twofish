ce1df8860dfeb45101ad9dd81df39f4fc69d619f
smurthy <smurthy@holy7c24602.rc.fas.harvard.edu>
2024-10-12 14:50:05 -0400

    slurm job script

--- run-models.sh
+++ run-models.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=zephyrGemma_concepts
+#SBATCH --job-name=tulu_concept
 #SBATCH --account=kempner_grads
-#SBATCH --partition=kempner
+#SBATCH --partition=kempner_requeue
+#SBATCH --constraint=h100
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=1
 #SBATCH --gpus-per-node=1
 #SBATCH --time=0-03:00
 #SBATCH --mem=100G
-#SBATCH --output=./sbatch-logs/zephyrGemma-%A_%a.out
-#SBATCH --error=./sbatch-logs/zephyrGemma-%A_%a.err
+#SBATCH --output=tulu-prompts-concepts-politicians-%A_%a.out
+#SBATCH --error=tulu-prompts-concepts-politicians-%A_%a.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=soniamurthy@g.harvard.edu
 #SBATCH --array=0-3  # Array indices for four jobs
@@ -23,15 +24,26 @@ conda activate jennlms
 conda deactivate
 conda activate jennlms
 
-# Define prompt conditions in an array
 PROMPT_CONDITIONS=("none" "random" "nonsense" "identity")
 
 # Run the job based on the array index
-python run_experiments.py --task="concepts" \
-  --concept_category="animals"\
-  --model_name="zephyr-gemma" \
-  --model_path="HuggingFaceH4/zephyr-7b-gemma-v0.1" \
+# no specified temperature = "default", add --temperature="1.5" or "2.0" (making sure prompt = "none")
+python run-experiments.py\
+  --task="concepts"\
+  --concept_category="politicians"\
+  --model_name="tulu"\
+  --model_path="allenai/tulu-2-7b"\
   --prompt_condition="${PROMPT_CONDITIONS[$SLURM_ARRAY_TASK_ID]}" \
-  --hf_token="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" \
-  --num_subjects=150 \
-  --batch_size=128
+  --hf_token="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"\
+  --batch_size=64
+
+
+python run-experiments.py\
+ --task="colors"\
+ --model_name="tulu"\
+ --model_path="allenai/tulu-2-7b"\
+ --prompt_condition="${PROMPT_CONDITIONS[$SLURM_ARRAY_TASK_ID]}"\
+ --hf_token="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"\
+ --num_words=199\
+ --num_subjects=150\
+ --batch_size=128
