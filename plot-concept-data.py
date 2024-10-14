c04329c83c190d8f8704abf2198aeb91dbd2d799
smurthy <smurthy@holy7c24304.rc.fas.harvard.edu>
2024-10-14 14:10:11 -0400

    concept task plots

--- plot-concept-data.py
+++ plot-concept-data.py
@@ -1,4 +1,3 @@
- # %%
 import pandas as pd
 import pickle
 import math
@@ -10,7 +9,7 @@ import argparse
 import os
 from tqdm import tqdm
 
-def plot_response_counts(df, metric):
+def plot_response_counts(models, df, metric):
     # combine the prompt and temperature columns
     df['combined'] = df['prompt'] + ", " + df['temperature'].astype(str)
     # remove human data from response_counts
@@ -23,7 +22,7 @@ def plot_response_counts(df, metric):
     custom_params = {"axes.spines.right": False, "axes.spines.top": False}
     sns.set_theme(style="ticks", rc=custom_params)
 
-    sns.barplot(x='model_name', y=f'{metric}_responses', hue='combined', hue_order=prompt_order, data=df, dodge=True)
+    sns.barplot(x='model_name', y=f'{metric}_responses', hue='combined', hue_order=prompt_order, data=df, dodge=True, order=models)
     
     plt.title(f"Number of {metric} responses per model, param combo")
     plt.xticks(rotation=45)
@@ -60,10 +59,8 @@ def plot_facetgrid_regplot(df, x_var, y_var, x_label, y_label, title):
 
 
 def plot_subplots(models, df, x_var, y_var, figure_dir, title, prompt, temperature):
-    
     if models == ['human']:
         fig, axs = plt.subplots(1, 1, figsize=(5, 5))
-        axs = [axs]  # Make axs a list for consistent handling
     else: 
         # Create a 2x5 grid of subplots
         fig, axs = plt.subplots(2, 5, figsize=(20, 10))
@@ -80,13 +77,17 @@ def plot_subplots(models, df, x_var, y_var, figure_dir, title, prompt, temperatu
     order = ["a finch", "a robin", "a chicken", "an eagle", "an ostrich", "a penguin", "a salmon", "a seal", "a dolphin", "a whale", "Abraham Lincoln", "Barack Obama", "Bernie Sanders", "Donald Trump", "Elizabeth Warren", "George W. Bush", "Hillary Clinton", "Joe Biden", "Richard Nixon", "Ronald Reagan"]
 
     for i, model in enumerate(models):
+        print(f"Plotting {model}...")
         model_data = df[df['model_name'] == model]
-        ax = axs[i]
 
         if models == ['human']:
-            sns.barplot(data=model_data, x=x_var, y=y_var, ax=ax, order=order, hue="concept_category", legend=False, dodge=True)        
+            ax = axs
+            print(model_data["Concept"])
+            # sns.barplot(data=model_data, x=x_var, y=y_var, ax=ax, order=order, hue="concept_category", hue_order=["animals", "politicians"], legend=False, dodge=True)        
         else: 
-            sns.barplot(data=model_data, x=x_var, y=y_var, ax=ax, err_kws={'linewidth': 0.7}, order=order,  hue="concept_category", dodge=True, legend=False)
+            ax = axs[i]
+
+        sns.barplot(data=model_data, x=x_var, y=y_var, ax=ax, err_kws={'linewidth': 0.7}, order=order,  hue="concept_category", hue_order=["animals", "politicians"], dodge=True, legend=False)
 
         ax.set_xlabel("Concept", fontsize=8)
         ax.set_ylabel("P(1 concept)", fontsize=8)
@@ -112,104 +113,42 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Plot data from color task")
 
     parser.add_argument("--plot", type=str, choices=["P(1 concept)", "response counts"], required=True, help="Which set of plots to generate")
-    parser.add_argument("--compile_data", type=bool, default=True, help="Whether to compile the data from the individual files")
 
     args = parser.parse_args()
 
-    # - - - - - - - - - - - - - - - - - - -
-    CRP_data_dir = "./concept-task/output-data-500iterations/"
-    CRP_output_dir = "./concept-task/output-data-500iterations/compiled/"
-    raw_data_dir = "./output-data/concept-task/"
-    raw_output_dir = "./output-data/concept-task/all/"
+    data_dir = "./output-data/concept-task/all/"
     # add subfolder for this plot type if it doesn't exist
     figure_dir = "./figures/concept-task/" + args.plot + "/"
     if not os.path.exists(figure_dir):
         os.makedirs(figure_dir)
 
-    # - - - - - - - - - - - - - - - - - - -
-    # Load data
-    models = ["starling", "openchat", "gemma-instruct", "zephyr-gemma", "mistral-instruct", "zephyr-mistral", "llama2", "llama2-chat", "tulu", "tulu-dpo"]
+    print("Loading compiled data...")
+    all_ClusteringResults = pd.read_pickle(f"{data_dir}all-ClusteringResults.pickle")
+    all_MAPTables = pd.read_pickle(f"{data_dir}all-MAPTables.pickle")
+    stats = pd.read_pickle(f"{data_dir}model-data.pickle")
+
     param_combos = [["none", "default"], ["none", "1.5"], ["none", "2.0"], ["random", "default"], ["nonsense", "default"], ["identity", "default"]]
-    results = ["ClusteringResults", "MAPTables"]
-    animals = ["a finch", "a robin", "a chicken", "an eagle", "an ostrich", "a penguin", "a salmon", "a seal", "a dolphin", "a whale"]
-    politicians = ["Abraham Lincoln", "Barack Obama", "Bernie Sanders", "Donald Trump", "Elizabeth Warren", "George W. Bush", "Hillary Clinton", "Joe Biden", "Richard Nixon", "Ronald Reagan"] 
-
-
-    # if output_dir doesn't exist, compile the data
-    if args.compile_data:
-        print("Compiling data...")
-        # add to the data frame
-        all_model_ClusteringResults = pd.DataFrame()
-        all_model_MAPTables = pd.DataFrame()
-        all_ClusteringResults = pd.DataFrame()
-        all_MAPTables = pd.DataFrame()
-        all_data = pd.DataFrame()
-        for model in models:
-            df_raw = pd.read_pickle(f"{raw_data_dir}concept-{model}.pickle")
-            all_data = pd.concat([all_data, df_raw])
-
-            for prompt, temperature in param_combos:
-                for result in results:
-                    # open the csv file for the model, prompt, and temperature combination
-                    # add column for each parameter to the dataframe
-                    df = pd.read_csv(f"{CRP_data_dir}{model}-prompt={prompt}-temp={temperature}-{result}.csv")
-                    df['model_name'] = model
-                    df['prompt'] = prompt
-                    df['temperature'] = temperature
-
-                    # check if concept is in animals or politicians and add column to df for concept type
-                    df["concept_category"] = np.where(df["Concept"].isin(animals), "animals", "politicians")
-
-                    if result == "ClusteringResults":
-                        # select data for last iteration
-                        df = df[df["Iteration"] == 499]
-                        all_model_ClusteringResults = pd.concat([all_model_ClusteringResults, df])
-                    else:
-                        all_model_MAPTables = pd.concat([all_model_MAPTables, df])
-        
-            # save compiled version of the data to output_dir
-            all_model_ClusteringResults.to_csv(f"{CRP_output_dir}{model}-ClusteringResults.csv", index=False)
-            all_ClusteringResults = pd.concat([all_ClusteringResults, all_model_ClusteringResults])
-            all_model_MAPTables.to_csv(f"{CRP_output_dir}{model}-MAPTables.csv", index=False)
-            all_MAPTables = pd.concat([all_MAPTables, all_model_MAPTables])
-
-        all_data.to_pickle(f"{raw_output_dir}all-data.pickle")
-        all_ClusteringResults.to_pickle(f"{raw_output_dir}all-ClusteringResults.pickle")
-        all_MAPTables.to_pickle(f"{raw_output_dir}all-MAPTables.pickle")
-            
-    else:
-        print("Loading compiled data...")
-        all_ClusteringResults = pd.read_pickle(f"{raw_output_dir}all-ClusteringResults.pickle")
-        all_MAPTables = pd.read_pickle(f"{raw_output_dir}all-MAPTables.pickle")
-        all_data = pd.read_pickle(f"{raw_output_dir}all-data.pickle")
-
 
     if args.plot == "P(1 concept)":
-        models = ["openchat","mistral-instruct", "gemma-instruct", "llama2", "tulu", 
-                  "starling", "zephyr-mistral", "zephyr-gemma", "llama2-chat", "tulu-dpo"]
-        for params in param_combos:
-            prompt, temperature = params
-            df = all_ClusteringResults[(all_ClusteringResults['prompt'] == prompt) & (all_ClusteringResults['temperature'] == temperature)]
-            # plot # of subjects sampled vs probability of same table (concept)
-            plot_subplots(models, all_model_ClusteringResults, "Concept", "ProbabilityOfSameTable",
-                        figure_dir, "P(1 concept)", prompt, temperature)
+        print("Plotting P(1 concept)...")
+        models = ["human"]
+        plot_subplots(models, all_ClusteringResults, "Concept", "ProbabilityOfSameTable",
+                    figure_dir, "P(1 concept)", "na", "na")
+        
+        # models = ["openchat","mistral-instruct", "gemma-instruct", "llama2", "tulu", 
+        #           "starling", "zephyr-mistral", "zephyr-gemma", "llama2-chat", "tulu-dpo"]
+        # for params in param_combos:
+        #     prompt, temperature = params
+        #     df = all_ClusteringResults[(all_ClusteringResults['prompt'] == prompt) & (all_ClusteringResults['temperature'] == temperature)]
+        #     # plot # of subjects sampled vs probability of same table (concept)
+        #     plot_subplots(models, all_ClusteringResults, "Concept", "ProbabilityOfSameTable",
+        #                 figure_dir, "P(1 concept)", prompt, temperature)
         
     elif args.plot == "response counts":
-        # group by model_name, prompt, temperature, and count number of responses
-        group = all_data.groupby(['model_name', 'prompt', 'temperature'])
-        all_data['total_responses'] = group['answer1'].transform('count')
-        
-        # create a mask for invalid responses where either answer1 or answer2 is -1
-        mask = (all_data['answer1'] != -1) & (all_data['answer2'] != -1)
-        all_data['valid_responses'] = mask
-        # group by model_name, prompt, temperature, and count number of valid responses
-        group = all_data.groupby(['model_name', 'prompt', 'temperature'])
-        all_data['valid_responses'] = group['valid_responses'].transform('sum')
-        all_data['invalid_responses'] = all_data['total_responses'] - all_data['valid_responses']
-        
-        print(all_data.columns)
-
-        plot_response_counts(all_data, "valid")
+        print("Plotting response counts...")
+        models = ["openchat","mistral-instruct", "gemma-instruct", "llama2", "tulu", 
+                  "starling", "zephyr-mistral", "zephyr-gemma", "llama2-chat", "tulu-dpo"]
+        plot_response_counts(models, stats, "valid")
         
 
 
